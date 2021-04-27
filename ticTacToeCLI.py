66e4c21358a5bc6443b7f2dc67c9d81d3591c825
Dominik Haeusser <haeusser-dominik@web.de>
2021-04-27 20:33:06 +0200

    first commit

---
+++ ticTacToeCLI.py
@@ -0,0 +1,15 @@
+def matchfield():
+    print("|X|O|O|")
+    print("|O|X|O|")
+    print("|O|O|X|")
+
+
+def main():
+    list = ["O","O","O","O","O","O","O","O","O"]
+    dies = (element for element in list)
+    print(dies)
+    
+
+
+if __name__ == '__main__':
+    main()
