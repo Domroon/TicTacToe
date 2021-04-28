791d8c61380bbfc19065a336af092d8de9595f21
Dominik Haeusser <haeusser-dominik@web.de>
2021-04-28 09:40:13 +0200

    add 'get_choices_list()'

--- ticTacToeCLI.py
+++ ticTacToeCLI.py
@@ -1,16 +1,37 @@
-def print_matchfield(choices):
+def print_matchfield(choices_list):
     output_string = ""
-    for choice in range(0, len(choices)):
-        output_string += "|" + choices[choice]
+    for choice in range(0, len(choices_list)):
+        output_string += "|" + choices_list[choice]
         if (choice + 1) % 3 == 0:
             output_string += "|\n" 
     print(output_string)
 
-def main():
-    choices = ["-","-","-","-","-","-","-","-","-"]
-    print_matchfield(choices)
-    
 
+def get_choices_list(player_1_choices, player_2_choices):
+    choices_list = ""
+    for choice in range(0, len(player_1_choices)):
+        if player_1_choices[choice] == False and player_2_choices[choice] == False:
+            choices_list += "-"
+
+        if player_1_choices[choice] == True:
+            choices_list += "X"
+
+        if player_2_choices[choice] == True:
+            choices_list += "O"
+
+        if player_1_choices[choice] == True and player_2_choices[choice] == True:
+            raise ValueError
+
+    return choices_list
+
+
+def main():
+    player_1_choices = [True] + [True] + [False for x in range(0, 7)]
+    player_2_choices = [False for x in range(0, 9)]
+    player_2_choices[4] = True
+    player_2_choices[6] = True
+    print_matchfield(get_choices_list(player_1_choices,player_2_choices))
+    
 
 if __name__ == '__main__':
     main()
