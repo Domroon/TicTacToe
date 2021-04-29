d85bd8353ef745aabb42485c5f7629b1d78ee56a
Dominik Haeusser <haeusser-dominik@web.de>
2021-04-29 12:17:12 +0200

    delete unecessary functions

--- ticTacToeCLI.py
+++ ticTacToeCLI.py
@@ -29,40 +29,6 @@ class Player:
     def add_sign(self, choices, position):
         choices[position] = self.sign
 
-    
-def print_matchfield(choices_list):
-    output_string = ""
-    for choice in range(0, len(choices_list)):
-        output_string += "|" + choices_list[choice]
-        if (choice + 1) % 3 == 0:
-            output_string += "|\n" 
-    print(output_string)
-
-
-def make_choices_list(player_1_choices, player_2_choices):
-    choices_list = ""
-    for choice in range(0, len(player_1_choices)):
-        if player_1_choices[choice] == False and player_2_choices[choice] == False:
-            choices_list += "-"
-        elif player_1_choices[choice] == True:
-            choices_list += "X"
-        elif player_2_choices[choice] == True:
-            choices_list += "O"
-        else:
-            raise ValueError
-
-    return choices_list
-
-
-def verify_choice(choice, player_1_choices, player_2_choices):
-    choice = int(choice)
-    if not 0 <= choice <= len(player_1_choices):
-        return False
-    elif player_1_choices[choice] == True and player_2_choices[choice] == True:
-        return False
-    else:
-        return True
-
 
 def main():
     matchfield = Matchfield()
