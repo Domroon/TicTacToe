1738f74cf417d568ccf7c28dc08a6aa81bab6427
Dominik Haeusser <haeusser-dominik@web.de>
2021-05-03 09:01:51 +0200

    add WIN_INDICES

--- ticTacToeCLI.py
+++ ticTacToeCLI.py
@@ -1,3 +1,10 @@
+WIN_INDICES = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], # horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], # vertical
+    [0, 4, 8], [2, 4, 6]             # diagonal
+]
+
+
 class Game:
     def __init__(self):
         self.matchfield = Matchfield()
